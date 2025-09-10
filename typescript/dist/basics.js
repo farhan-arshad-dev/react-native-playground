e3ba7bfb1a30efb42575e48f243547b7772d116c
Farhan Arshad <farhan.arshad@arbisoft.com>
2025-09-10 21:26:00 +0500

    Set up TypeScript in playground

---
+++ typescript/dist/basics.js
@@ -0,0 +1,4 @@
+let id = 5;
+console.log(`Id = ${id}`);
+export {};
+//# sourceMappingURL=basics.js.map
---
+++ typescript/src/basics.ts
@@ -0,0 +1,22 @@
+// Typescript opensource
+// Superset of Java Script
+// types are optional
+// complies down to regular JS
+// Can use for both front-end and back-end (with Node.js)
+// 3rd party libraries can be added for type defination
+
+// Dynamic Typing => associated with runtime. (Java Script, Python, Ruby, PHP)
+// Static Typing => explicitly assign to variables, funcation, params, return values e.t.c (Java, C/C++, Rust, Go)
+
+// PROS => More Robust, Easily Spot Bugs, Predictability, Readability, Popular
+// Cons => More code to white, More to learn, Required compilation, Not true static type.
+// Extensions .ts and .tsx.
+// TSC (TypeScript Compiler) is used to complete .ts files down to JS.
+// Report at compile time. Many TS compilation tools.
+// tsconfig.json file is used to configure how TypeScript works.
+// User tsc --init to create the default `tsconfig.json`
+
+let id: number = 5;
+console.log(`Id = ${id}`);
+
+// Basics Types
