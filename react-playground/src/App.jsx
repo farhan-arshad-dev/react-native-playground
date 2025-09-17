9fc0e333e846e517bdb516c5a79763b689ab7b79
Farhan Arshad <farhan.arshad@arbisoft.com>
2025-09-17 19:43:09 +0500

    - your-first-component at react.dev
    including 4 challenges in link: https://react.dev/learn/your-first-component

--- react-playground/src/App.jsx
+++ react-playground/src/App.jsx
@@ -1,6 +1,23 @@
 function App() {
   return (
-    <div>React Playground</div>
+    // should return single element like div OR empty html tag.
+    <>
+      <section>
+        <div>React Playground</div>
+        <p>Hi! this is Katherine Johnson.</p>
+        <Profile />
+        <Profile />
+      </section>
+    </>
+  )
+}
+
+export function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
   )
 }
 
